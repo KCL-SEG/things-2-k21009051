d2547191f33af271ad8b3788951adfababeecf0c
Saskia Lablans <k21009051@kcl.ac.uk>
2022-10-14 11:43:19 +0100

    Create class Meta to fix errors

--- thingproject/urls.py
+++ thingproject/urls.py
@@ -19,5 +19,6 @@ from things import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='ThingForm'),
+    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
 ]
--- things/forms.py
+++ things/forms.py
@@ -2,18 +2,9 @@ from django import forms
 from django.core.validators import MinValueValidator, MaxValueValidator
 from .models import Thing
 
-class ThingForm(forms.Form):
+class ThingForm(forms.ModelForm):
+	class Meta:
+		model = Thing
+		fields = ['name','description','quantity']
+		widgets = {'description' : forms.Textarea()}
 
-	name = forms.CharField(
-		label = 'Name',
-		max_length = 35)
-
-	description = forms.CharField(
-		label = 'Description',
-		widget = forms.Textarea(),
-		max_length=120)
-
-	quantity = forms.CharField(
-		label='Quantity',
-		widget = forms.NumberInput(),
-		validators = [[MinValueValidator(0),MaxValueValidator(50)]])
