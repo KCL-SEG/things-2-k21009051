d38f4730d1414ceda91ab531eed38f909f5e9a91
Saskia Lablans <k21009051@kcl.ac.uk>
2022-10-14 10:34:02 +0100

    Add form to home page

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,19 @@
-"""Forms of the project."""
+from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator
+from .models import Thing
 
-# Create your forms here.
+class ThingForm(forms.Form):
+
+	name = forms.CharField(
+		label = 'Name',
+		max_length = 35)
+
+	description = forms.CharField(
+		label = 'Description',
+		widget = forms.Textarea(),
+		max_length=120)
+
+	quantity = forms.CharField(
+		label='Quantity',
+		widget = forms.NumberInput(),
+		validators = [[MinValueValidator(0),MaxValueValidator(50)]])
